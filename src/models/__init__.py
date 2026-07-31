20a0f0c5ee95d84e7f045814742ede7be5244ea1
jie.yuan <yuanjielovejesus@gmail.com>
2026-07-31 14:57:25 +0200

    refactor: regressively clean obsolete entry scripts, debug files, and update model exports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,5 @@
-# Models package initialization
+from src.models.factory import build_model
+from src.models.detr import DETR
+from src.models.slot_attention import build_savi_model as StoSAVi
+
+__all__ = ["build_model", "DETR", "StoSAVi"]
